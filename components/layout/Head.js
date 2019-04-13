00a847595296f5c54b145cdb7778e77b7c04cba4
rizkiandrianto <rizki_andrianto@rocketmail.com>
2019-04-13 22:26:16 +0700

    :sparkles: Add FavIcon

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -4,6 +4,7 @@ import Style from '../../styles/bundle.scss';
 export default ({ children, defaultStyle= true }) => (
   <Head>
     <title>Sorabel</title>
+    <link rel="icon" href="/static/images/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     {defaultStyle && <style dangerouslySetInnerHTML={{ __html: Style }} />}
     {children}
