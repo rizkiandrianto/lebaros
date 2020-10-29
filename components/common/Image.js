272012a534647d499fa533d36a51f05d5c2b9e1a
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2020-10-29 20:03:28 +0700

    :sparkles: Add EN and ID locale

--- components/common/Image.js
+++ components/common/Image.js
@@ -1,7 +1,6 @@
 import NextImage from 'next/image';
 
 export default function Image({ className, src, width, height, ...arg }) {
-  if (arg && Object.keys(arg).length) console.log(arg)
   if (src.startsWith('data:image')) return <img src={src} width={width} className={className} {...arg} />
   return <NextImage src={src} width={width} height={height} className={className} key={src} {...arg} />
 }
