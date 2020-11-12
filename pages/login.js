7241d24db501cf77b6ae4f623da21dc1ecd79fdf
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2020-11-12 21:36:02 +0700

    :wrench: Remove login page

--- pages/login.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import LoginForm from '../components/common/Form/LoginForm';
-const Login = () => {
-  return (
-    <LoginForm />
-  )
-}
-
-export default Login;
